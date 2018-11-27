cfcfd50f4f0560d62bdfe4afd84fc66664ac1936
Unknown <joel.andrewsd@gmail.com>
2018-11-26 21:06:10 -0600

    Added script for task page

--- Todo/App_Start/BundleConfig.cs
+++ Todo/App_Start/BundleConfig.cs
@@ -14,6 +14,8 @@ namespace Todo
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/ko").Include(
+                        "~/Scripts/knockout-3.4.2.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +28,9 @@ namespace Todo
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/taskScripts").Include(
+                      "~/Scripts/TaskViewModel.js"));
         }
     }
 }
---
+++ Todo/Scripts/TaskViewModel.ts
@@ -0,0 +1,9 @@
+﻿module Task {
+    export class TaskViewModel{
+        public TaskEntered: KnockoutObservable<string> = ko.observable('');
+
+        public checkTask() {
+            console.log(this.TaskEntered());
+        }
+    }
+}
